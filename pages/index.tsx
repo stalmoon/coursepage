f5399f7974c40e1a20d2e69bbd2ce1768ab31c36
ThalesBMC <thalesbmc@gmail.com>
2021-03-22 19:57:34 -0300

    cleaning

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Course website</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Course</div>
+    </div>
+  )
+}
